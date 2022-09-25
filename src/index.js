28e5df1e1a6812c05b45c9f64a656462552124f3
Mahesh Gareja <vvpmahesh@gmail.com>
2022-09-25 17:27:59 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -9,9 +9,6 @@ const responseGoogle = response => {
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-
       <GoogleLogin
         clientId="796074674147-bu1oat7714eglvljkdpo4hfmcghd1a8n.apps.googleusercontent.com"
         buttonText="Login"
