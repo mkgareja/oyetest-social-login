6d1798096ff05ba879d69358a78663df3d873539
Mahesh Gareja <vvpmahesh@gmail.com>
2022-09-25 22:39:56 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -6,7 +6,28 @@ const responseGoogle = response => {
   // console.log(response);
   console.log("Login Success: currentUser:", response);
   window.parent.postMessage({ message: "getAppData", value: response }, "*");
+  refreshTokenSetup(res);
 };
+
+const refreshTokenSetup = (res) => {
+  // Timing to renew access token
+  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
+
+  const refreshToken = async () => {
+    const newAuthRes = await res.reloadAuthResponse();
+    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
+    console.log('newAuthRes:', newAuthRes);
+    // saveUserToken(newAuthRes.access_token);  <-- save new token
+    localStorage.setItem('authToken', newAuthRes.id_token);
+
+    // Setup the other timer after the first one
+    setTimeout(refreshToken, refreshTiming);
+  };
+
+  // Setup first refresh timer
+  setTimeout(refreshToken, refreshTiming);
+};
+
 
 
 function App() {
