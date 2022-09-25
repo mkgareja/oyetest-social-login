08dbd13edfec253650286f06f6dc2e686d9c758f
Mahesh Gareja <vvpmahesh@gmail.com>
2022-09-25 11:32:20 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+// Import Parse minified version
+import Parse from 'parse/dist/parse.min.js';
+
+// Your Parse initialization configuration goes here
+const PARSE_APPLICATION_ID = '0VWhf3TtEH0dubHhFEp5V9y0dxyXKTOL53oaOtm5';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'ADeVUrFJiooeqp4geOxS3Y4f25oTCTbThehuG4n4';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
   return (
